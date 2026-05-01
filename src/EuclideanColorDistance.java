0db0321b83c58ef378e97d1d6eadc888f63687b3
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-04-30 22:02:49 -0700

    Finished Wave 4

--- src/EuclideanColorDistance.java
+++ src/EuclideanColorDistance.java
@@ -19,6 +19,40 @@ public class EuclideanColorDistance implements ColorDistanceFinder {
      */
     @Override
     public double distance(int colorA, int colorB) {
-        return 0;
+        if (colorA < 0 || colorA > 0xFFFFFF ||
+            colorB < 0 || colorB > 0xFFFFFF) {
+            throw new IllegalArgumentException();
+        }
+
+        return hexToRGB(colorA, colorB);
+    }
+
+    private static double hexToRGB(int colorA, int colorB) {
+        // Grab the r1 b1 g1 (colorA)
+        double r1 = (colorA >> 16) & 0xFF;
+        double g1 = (colorA >> 8) & 0xFF;
+        double b1 = colorA & 0xFF;
+
+        // Grab the r2 b2 g2 (colorB)
+        double r2 = (colorB >> 16) & 0xFF;
+        double g2 = (colorB >> 8) & 0xFF;
+        double b2 = colorB & 0xFF;
+
+        // Euclidean Equation
+        // subtract
+        double rValue = r1 - r2;
+        double gValue = g1 - g2;
+        double bValue = b1 - b2;
+
+        // square
+        rValue = rValue * rValue;
+        gValue = gValue * gValue;
+        bValue = bValue * bValue;
+
+        // sum
+        double sumValue = rValue + gValue + bValue;
+
+        // sqaure
+        return Math.sqrt(sumValue);
     }
 }
