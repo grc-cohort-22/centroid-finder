b40149476cb391413546eab58513fcd1b973f4dd
Azeb Tesfay <azbseb@gmail.com>
2026-05-01 20:25:32 -0700

    AI Used cod for test testDfsBinaryGroupFinderSingleGroup

--- src/DfsBinaryGroupFinderTest.java
+++ src/DfsBinaryGroupFinderTest.java
@@ -9,15 +9,35 @@ public class DfsBinaryGroupFinderTest {
     @Test
     public void testSinglePixelGroup() {
         int[][] image = {
-            {1}
+                { 1 }
         };
 
         DfsBinaryGroupFinder finder = new DfsBinaryGroupFinder();
 
         List<Group> expected = List.of(
-            new Group(1, new Coordinate(0, 0))
-        );
+                new Group(1, new Coordinate(0, 0)));
 
         assertEquals(expected, finder.findConnectedGroups(image));
+    }
+
+    @Test
+    public void testDfsBinaryGroupFinderSingleGroup() {
+        int[][] image = {
+                { 1, 1, 0 },
+                { 0, 1, 0 },
+                { 0, 0, 0 }
+        };
+
+        BinaryGroupFinder finder = new DfsBinaryGroupFinder();
+
+        List<Group> groups = finder.findConnectedGroups(image);
+
+        assertEquals(1, groups.size());
+
+        Group group = groups.get(0);
+
+        assertEquals(3, group.size());
+        assertEquals(0, group.centroid().x());
+        assertEquals(0, group.centroid().y());
     }
 }
