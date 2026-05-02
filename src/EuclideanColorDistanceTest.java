f48a3f7f91b0d5ba37d8a73268a59c8e1a375c67
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2026-05-01 21:54:09 -0700

    Implemented Wave 3 - EuclideanColorDistance and fixed indentation on remaining tests

--- src/EuclideanColorDistanceTest.java
+++ src/EuclideanColorDistanceTest.java
@@ -56,46 +56,46 @@ public class EuclideanColorDistanceTest {
 
 
 
-      @Test
-    public void testGreenToBlue() {
-        double expected = Math.sqrt(255 * 255 + 255 * 255);
+  @Test
+  public void testGreenToBlue() {
+    double expected = Math.sqrt(255 * 255 + 255 * 255);
 
-        assertEquals(expected, finder.distance(0x00FF00, 0x0000FF), EPSILON);
-    }
+    assertEquals(expected, finder.distance(0x00FF00, 0x0000FF), EPSILON);
+  }
 
-    @Test
-    public void testSmallDifferenceOnlyInRed() {
-        assertEquals(1.0, finder.distance(0x010000, 0x000000), EPSILON);
-    }
+  @Test
+  public void testSmallDifferenceOnlyInRed() {
+    assertEquals(1.0, finder.distance(0x010000, 0x000000), EPSILON);
+  }
 
-    @Test
-    public void testSmallDifferenceOnlyInGreen() {
-        assertEquals(1.0, finder.distance(0x000100, 0x000000), EPSILON);
-    }
+  @Test
+  public void testSmallDifferenceOnlyInGreen() {
+    assertEquals(1.0, finder.distance(0x000100, 0x000000), EPSILON);
+  }
 
-    @Test
-    public void testSmallDifferenceOnlyInBlue() {
-        assertEquals(1.0, finder.distance(0x000001, 0x000000), EPSILON);
-    }
+  @Test
+  public void testSmallDifferenceOnlyInBlue() {
+    assertEquals(1.0, finder.distance(0x000001, 0x000000), EPSILON);
+  }
 
-    @Test
-    public void testMixedColorDistance() {
-        // colorA = (18, 52, 86)
-        // colorB = (101, 67, 33)
-        double expected = Math.sqrt(
-            Math.pow(18 - 101, 2) +
-            Math.pow(52 - 67, 2) +
-            Math.pow(86 - 33, 2)
-        );
+  @Test
+  public void testMixedColorDistance() {
+    // colorA = (18, 52, 86)
+    // colorB = (101, 67, 33)
+    double expected = Math.sqrt(
+      Math.pow(18 - 101, 2) +
+      Math.pow(52 - 67, 2) +
+      Math.pow(86 - 33, 2)
+    );
 
-        assertEquals(expected, finder.distance(0x123456, 0x654321), EPSILON);
-    }
+    assertEquals(expected, finder.distance(0x123456, 0x654321), EPSILON);
+  }
 
-    @Test
-    public void testDistanceIsSymmetric() {
-        double distanceAB = finder.distance(0x123456, 0xABCDEF);
-        double distanceBA = finder.distance(0xABCDEF, 0x123456);
+  @Test
+  public void testDistanceIsSymmetric() {
+    double distanceAB = finder.distance(0x123456, 0xABCDEF);
+    double distanceBA = finder.distance(0xABCDEF, 0x123456);
 
-        assertEquals(distanceAB, distanceBA, EPSILON);
-    }
+    assertEquals(distanceAB, distanceBA, EPSILON);
+  }
 }
