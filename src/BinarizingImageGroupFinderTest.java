d524277d4e8cee8259a77e5ae72e3f2beada8d4c
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-05-03 06:42:30 -0700

    AI Used to test BinarizingImageGroupFinder

--- src/BinarizingImageGroupFinderTest.java
+++ src/BinarizingImageGroupFinderTest.java
@@ -31,4 +31,79 @@ public class BinarizingImageGroupFinderTest {
   assertEquals(new Group(3, new Coordinate(0, 0)), groups.get(0));
  }
 
+@Test
+    public void testFindConnectedGroupsReturnsGroupsFromBinaryGroupFinder() {
+        ImageBinarizer fakeBinarizer = new FakeImageBinarizer();
+        BinaryGroupFinder fakeGroupFinder = new FakeBinaryGroupFinder();
+
+        BinarizingImageGroupFinder finder =
+            new BinarizingImageGroupFinder(fakeBinarizer, fakeGroupFinder);
+
+        BufferedImage image = new BufferedImage(2, 2, BufferedImage.TYPE_INT_RGB);
+
+        List<Group> expected = List.of(
+            new Group(2, new Coordinate(0, 0))
+        );
+
+        assertEquals(expected, finder.findConnectedGroups(image));
+    }
+
+    @Test
+    public void testFakeBinarizerCreatesBinaryArrayUsedByGroupFinder() {
+        ImageBinarizer fakeBinarizer = new FakeImageBinarizer();
+        CheckingBinaryGroupFinder checkingGroupFinder = new CheckingBinaryGroupFinder();
+
+        BinarizingImageGroupFinder finder =
+            new BinarizingImageGroupFinder(fakeBinarizer, checkingGroupFinder);
+
+        BufferedImage image = new BufferedImage(2, 2, BufferedImage.TYPE_INT_RGB);
+
+        finder.findConnectedGroups(image);
+
+        assertTrue(checkingGroupFinder.receivedExpectedArray);
+    }
+
+    private static class FakeImageBinarizer implements ImageBinarizer {
+
+        @Override
+        public int[][] toBinaryArray(BufferedImage image) {
+            return new int[][] {
+                {1, 0},
+                {0, 1}
+            };
+        }
+
+        @Override
+        public BufferedImage toBufferedImage(int[][] image) {
+            return null;
+        }
+    }
+
+    private static class FakeBinaryGroupFinder implements BinaryGroupFinder {
+
+        @Override
+        public List<Group> findConnectedGroups(int[][] image) {
+            return List.of(
+                new Group(2, new Coordinate(0, 0))
+            );
+        }
+    }
+
+    private static class CheckingBinaryGroupFinder implements BinaryGroupFinder {
+        boolean receivedExpectedArray = false;
+
+        @Override
+        public List<Group> findConnectedGroups(int[][] image) {
+            if (image[0][0] == 1 &&
+                image[0][1] == 0 &&
+                image[1][0] == 0 &&
+                image[1][1] == 1) {
+                receivedExpectedArray = true;
+            }
+
+            return List.of();
+        }
+    }
 }
+
+
